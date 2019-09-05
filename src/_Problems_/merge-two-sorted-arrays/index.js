86c977977ac44f4873e5797ae8912a6fad9bef41
Ashok Dey <ashokdey100@gmail.com>
2019-09-05 12:37:43 +0530

    --update: code for 'merge 2 sorted arrays'

---
+++ src/_Problems_/merge-two-sorted-arrays/index.js
@@ -0,0 +1,40 @@
+// the running complexity O(n + m) where n and m are the length of 2 arrays
+function mergeTwoSortedArrays(arr1, arr2) {
+  let i = 0;
+  let j = 0;
+
+  const merged = [];
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      merged.push(arr1[i]);
+      i += 1;
+    } else {
+      merged.push(arr2[j]);
+      j += 1;
+    }
+  }
+
+  // if still elments are left in arr1
+  if (i < arr1.length) {
+    while (i < arr1.length) {
+      merged.push(arr1[i]);
+      i += 1;
+    }
+  }
+
+  // if still elments are left in arr2
+  if (j < arr2.length) {
+    while (j < arr2.length) {
+      merged.push(arr2[j]);
+      j += 1;
+    }
+  }
+
+  return merged;
+}
+
+// since we are using sort here, the running time complexity will be O(nlogn)
+function mergeTwoSortedArrays2(arr1, arr2) {
+  return [...arr1, ...arr2].sort((a, b) => a - b);
+}
