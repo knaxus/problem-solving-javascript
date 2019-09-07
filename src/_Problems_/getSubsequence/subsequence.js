896bac7f6b65a862ffa20f3a4a5a3ec4aab1a1a6
navneet <navneet_krishna@outlook.com>
2019-09-07 20:40:00 +0530

    GET PERMUTATION AND SUBSEQUENCE OF A GIVEN STRING

---
+++ src/_Problems_/getSubsequence/subsequence.js
@@ -0,0 +1,28 @@
+// FIND SUBSEQUENCE OF A GIVEN SUBSTRING
+// SUBSTRING OF 'abc' ---->>>>  [ '', 'a', 'b', 'ab', 'c', 'ac', 'bc', 'abc' ]
+// SUBSTRING OF 'bc'  ---->>>>  ['', 'b', 'c', 'bc']
+// SUBSTRING OF 'c'   ---->>>>  ['', 'c']
+// A pattern can be noticed in above three substrings. Technique followed is recursion.
+
+
+let getSubesequence = (str) => {
+    if (str.length == 0) {
+        let array = [''];
+        return array;
+    }
+
+    let currentChar = str.charAt(0);
+    let restOfString = str.substring(1);
+
+    let result = [];
+    let returnResult = getSubesequence(restOfString);
+    for (i = 0; i < returnResult.length; i++) {
+        result.push(returnResult[i]);
+        result.push(currentChar + returnResult[i]);
+    }
+    return result;
+}
+
+
+let subsequence = getSubesequence('abc');
+console.log(subsequence);
