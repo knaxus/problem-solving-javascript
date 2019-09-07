95c9cf0ddd7c0b0da37db6787c454c2b11c37b51
Ashok Dey <ashokdey100@gmail.com>
2019-09-07 13:43:13 +0530

    --update: added test cases

--- src/_Problems_/product-of-elements/index.js
+++ src/_Problems_/product-of-elements/index.js
@@ -1,5 +1,13 @@
 // An array such that each index has a product of all the numbers in the array except the number stored at that index.
 
+/**
+ * Input - [1, 2, 3, 4]
+ * Output - [24, 12, 8, 6]
+ *
+ * Input - [0, 1, 3, 5]
+ * Output - [15, 0, 0, 0]
+ */
+
 function findProduct(arr) {
   let left = 1;
   const result = [];
