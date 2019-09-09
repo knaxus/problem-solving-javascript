479e92582cf87503b84a5ca096fb8ff817357ed4
Ashok Dey <ashokdey100@gmail.com>
2019-09-09 13:00:15 +0530

    --update: get pair of index for two numbers sum to N

--- src/_Problems_/find-2-nums-adding-to-n/index.js
+++ src/_Problems_/find-2-nums-adding-to-n/index.js
@@ -16,6 +16,20 @@ function findTwoNumsAddingToN(arr, number) {
   return pair.length ? pair : false;
 }
 
+function findIndicesOfTwoNumsAddingToN(nums, target) {
+  const store = new Set();
+  const pair = [];
+  for (let index in nums) {
+    if (store.has(target - nums[index])) {
+      pair.push(nums.indexOf(target - nums[index]));
+      pair.push(index);
+      break;
+    }
+    store.add(nums[index]);
+  }
+  return pair.length ? pair : false;
+}
+
 // the Brute force approach
 function findTwoNumsAddingToN2(arr, number) {
   const pair = [];
