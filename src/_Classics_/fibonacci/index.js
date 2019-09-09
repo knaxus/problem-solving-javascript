46dcf702cef37a683a190f901eb0478dc5abc93a
Ashok Dey <ashokdey100@gmail.com>
2019-09-09 17:30:53 +0530

    --update: added optimized fibonacci using cache

--- src/_Classics_/fibonacci/index.js
+++ src/_Classics_/fibonacci/index.js
@@ -8,3 +8,29 @@ function fibonacci(position) {
   // element at position(p) (p -1) and (p - 2)
   return fibonacci(position - 2) + fibonacci(position - 1);
 }
+
+/**
+ * Memoization. In computing, memoization or memoisation is an
+ * optimization technique used primarily to speed up computer
+ * programs by storing the results of expensive function
+ * calls and returning the cached result when the
+ * same inputs occur again
+ */
+
+// Linear time, test with index as 510 for both the functions
+function fibonacciMemoized(index, cache) {
+  cache = cache || [];
+
+  if (cache[index]) {
+    return cache[index];
+  } else {
+    if (index < 3) {
+      return 1;
+    } else {
+      cache[index] =
+        fibonacciMemoized(index - 1, cache) +
+        fibonacciMemoized(index - 2, cache);
+    }
+  }
+  return cache[index];
+}
