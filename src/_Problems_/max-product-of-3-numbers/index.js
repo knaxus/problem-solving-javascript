5458f1f548afe7b06ab35b8dad79cc00c0b9f9e0
Ashok Dey <ashokdey100@gmail.com>
2019-09-28 10:42:28 +0530

    update: solution in O(n)

--- src/_Problems_/max-product-of-3-numbers/index.js
+++ src/_Problems_/max-product-of-3-numbers/index.js
@@ -24,5 +24,42 @@ function maxProductof3Numbers(arr) {
   // considering -ve numbers, 2 -ve on multiplication becomes +ve
   const p2 = arr[0] * arr[1] * arr[n - 1];
 
+  // return the largest of two probablities
+  return p1 > p2 ? p1 : p2;
+}
+
+/** The following solution is O(n) */
+function maxProductof3NumbersII(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error('Invalid Argument');
+  }
+
+  let firstMax = (secondMax = thirdMax = Number.MIN_SAFE_INTEGER);
+  let firstMin = (secondMin = Number.MAX_SAFE_INTEGER);
+
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i] > firstMax) {
+      thirdMax = secondMax;
+      secondMax = firstMax;
+      firstMax = arr[i];
+    } else if (arr[i] > secondMax) {
+      thirdMax = secondMax;
+      secondMax = arr[i];
+    } else if (arr[i] > thirdMax) {
+      thirdMax = arr[i];
+    }
+
+    // check for mins
+    if (arr[i] < firstMin) {
+      secondMin = firstMin;
+      firstMin = arr[i];
+    } else if (arr[i] < secondMin) {
+      secondMin = arr[i];
+    }
+  }
+
+  const p1 = firstMax * secondMax * thirdMax;
+  const p2 = firstMin * secondMin * firstMax;
+
   return p1 > p2 ? p1 : p2;
 }
