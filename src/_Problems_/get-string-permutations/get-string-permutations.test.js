26bbfbdd1b022d1aac03d28af0566a1d3e837d29
Luciano Medeiros Marcelino <lucianom@nulogy.com>
2019-10-03 20:27:11 -0400

    Add tests for get-string-permutation

--- src/_Problems_/get-string-permutations/get-string-permutations.test.js
+++ src/_Problems_/get-string-permutations/get-string-permutations.test.js
@@ -1,24 +1,31 @@
-// GET PERMUTAION OF A GIVEN STRING
+const { getPermutations } = require('.');
 
-let getPermutation = (str) => {
-    if (str.length == 1) {                          // BASE CASE
-        let array = [];
-        array.push(str);
-        return array;
-    }
+describe('Get permutations of a string', () => {
+  it('returns permutations of a short string', () => {
+    const shortString = 'ab';
+    const expectedPermutations = ['ab', 'ba'];
 
-    let currentCharacter = str.charAt(0);
-    let restOfString = str.substring(1);
-    let result = [];
-    let returnResult = getPermutation(restOfString);
-    for (j = 0; j < returnResult.length; j++) {
-        for (i = 0; i <= returnResult[j].length; i++) {
-            let value = returnResult[j].substring(0, i) + currentCharacter + returnResult[j].substring(i);
-            result.push(value);
-        }
-    }
-    return result;
-}
+    expect(getPermutations(shortString)).toEqual(expectedPermutations);
+  });
 
-let permutation = getPermutation('abc');
-console.log(permutation);
+  it('returns permutations of a long string', () => {
+    const shortString = 'XUNDA';
+    const expectedPermutations = ['XUNDA', 'UXNDA', 'NXUDA', 'XNUDA', 'UNXDA', 'NUXDA', 'DUXNA', 'UDXNA', 'XDUNA', 'DXUNA', 'UXDNA', 'XUDNA', 'XNDUA', 'NXDUA', 'DXNUA', 'XDNUA', 'NDXUA', 'DNXUA', 'DNUXA', 'NDUXA', 'UDNXA', 'DUNXA', 'NUDXA', 'UNDXA', 'ANDXU', 'NADXU', 'DANXU', 'ADNXU', 'NDAXU', 'DNAXU', 'XNADU', 'NXADU', 'AXNDU', 'XANDU', 'NAXDU', 'ANXDU', 'ADXNU', 'DAXNU', 'XADNU', 'AXDNU', 'DXANU', 'XDANU', 'XDNAU', 'DXNAU', 'NXDAU', 'XNDAU', 'DNXAU', 'NDXAU', 'UDXAN', 'DUXAN', 'XUDAN', 'UXDAN', 'DXUAN', 'XDUAN', 'ADUXN', 'DAUXN', 'UADXN', 'AUDXN', 'DUAXN', 'UDAXN', 'UXADN', 'XUADN', 'AUXDN', 'UAXDN', 'XAUDN', 'AXUDN', 'AXDUN', 'XADUN', 'DAXUN', 'ADXUN', 'XDAUN', 'DXAUN', 'NXAUD', 'XNAUD', 'ANXUD', 'NAXUD', 'XANUD', 'AXNUD', 'UXNAD', 'XUNAD', 'NUXAD', 'UNXAD', 'XNUAD', 'NXUAD', 'NAUXD', 'ANUXD', 'UNAXD', 'NUAXD', 'AUNXD', 'UANXD', 'UAXND', 'AUXND', 'XUAND', 'UXAND', 'AXUND', 'XAUND', 'DAUNX', 'ADUNX', 'UDANX', 'DUANX', 'AUDNX', 'UADNX', 'NADUX', 'ANDUX', 'DNAUX', 'NDAUX', 'ADNUX', 'DANUX', 'DUNAX', 'UDNAX', 'NDUAX', 'DNUAX', 'UNDAX', 'NUDAX', 'NUADX', 'UNADX', 'ANUDX', 'NAUDX', 'UANDX', 'AUNDX'];
+
+    expect(getPermutations(shortString).sort()).toEqual(expectedPermutations.sort());
+  });
+
+  it('returns the same string if the string is one character long', () => {
+    const shortString = 'a';
+    const expectedPermutations = ['a'];
+
+    expect(getPermutations(shortString)).toEqual(expectedPermutations);
+  });
+
+  it('is case sensitive', () => {
+    const shortString = 'aB';
+
+    expect(getPermutations(shortString)).not.toEqual(['ab', 'ba']);
+    expect(getPermutations(shortString)).toEqual(['aB', 'Ba']);
+  });
+});
