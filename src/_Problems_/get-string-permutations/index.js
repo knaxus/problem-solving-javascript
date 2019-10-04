ecf987a9a24120c1a14c53b0effd7c5aa6cf40da
Luciano Medeiros Marcelino <lucianom@nulogy.com>
2019-10-03 20:10:10 -0400

    Adjust file to keep consistency
    The whole repository uses 2 spaces for indentation and does not use
    console.log anywhere.
    Also the repo standard is to use dash to separate words in the folders
    names.

---
+++ src/_Problems_/get-string-permutations/index.js
@@ -0,0 +1,23 @@
+// GET PERMUTATION OF A GIVEN STRING
+
+let getPermutation = (str) => {
+  if (str.length == 1) {
+    let array = [];
+    array.push(str);
+    return array;
+  }
+
+  let currentCharacter = str.charAt(0);
+  let restOfString = str.substring(1);
+  let result = [];
+  let returnResult = getPermutation(restOfString);
+
+  for (j = 0; j < returnResult.length; j++) {
+    for (i = 0; i <= returnResult[j].length; i++) {
+      let value = returnResult[j].substring(0, i) + currentCharacter + returnResult[j].substring(i);
+      result.push(value);
+    }
+  }
+
+  return result;
+}
