471ac13c16462a82241dc044325d9d7d3348c7f2
deadem <deadem@gmail.com>
2019-10-04 10:10:32 +0300

    add subsequence tests

---
+++ src/_Problems_/get_subsequence/subsequence.test.js
@@ -0,0 +1,19 @@
+const { getSubesequence } = require('.');
+
+describe('GetSubesequence', () => {
+  it('Sequence of abc', () => {
+    expect(getSubesequence('abc').sort()).toEqual([ '', 'a', 'ab', 'abc', 'ac', 'b', 'bc', 'c' ]);
+  });
+
+  it('Sequence of bc', () => {
+    expect(getSubesequence('bc').sort()).toEqual(['', 'b', 'bc', 'c']);
+  });
+
+  it('Sequence of c', () => {
+    expect(getSubesequence('c').sort()).toEqual(['', 'c']);
+  });
+
+  it('Sequence of empty', () => {
+    expect(getSubesequence('').sort()).toEqual(['']);
+  });
+});
