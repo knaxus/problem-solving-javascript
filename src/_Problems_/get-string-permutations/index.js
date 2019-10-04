729bcef6115f6de60991cfdbef7f50bb161d276a
Luciano Medeiros Marcelino <lucianom@nulogy.com>
2019-10-03 20:29:21 -0400

    Cover case of an empty string in get-string-permutations
    When adding a test case for an empty string, the code was looping
    endlessly.

--- src/_Problems_/get-string-permutations/index.js
+++ src/_Problems_/get-string-permutations/index.js
@@ -1,15 +1,19 @@
 // GET PERMUTATION OF A GIVEN STRING
 
 const getPermutations = (str) => {
+  let result = [];
+
+  if (str.length == 0) {
+    return result;
+  }
+
   if (str.length == 1) {
-    let array = [];
-    array.push(str);
-    return array;
+    result.push(str);
+    return result;
   }
 
   let currentCharacter = str.charAt(0);
   let restOfString = str.substring(1);
-  let result = [];
   let returnResult = getPermutations(restOfString);
 
   for (j = 0; j < returnResult.length; j++) {
