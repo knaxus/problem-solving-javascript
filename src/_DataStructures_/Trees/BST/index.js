265d41f119720e5cea77d0f1c2ceca36c885ffa5
Ashok Dey <ashokdey100@gmail.com>
2019-10-08 13:27:28 +0530

    update: BST insertion added

---
+++ src/_DataStructures_/Trees/BST/index.js
@@ -0,0 +1,42 @@
+const Node = require('./Node');
+
+class BinarySearchTree {
+  constructor(value) {
+    this.root = new Node(value);
+  }
+
+  insert(root, value) {
+    if (root === null) {
+      const newNode = new Node(value);
+      // eslint-disable-next-line no-param-reassign
+      root = newNode;
+      return root;
+    }
+
+    if (value < root.value) {
+      // go left
+      // eslint-disable-next-line no-param-reassign
+      root.leftChild = this.insert(root.leftChild, value);
+      return root;
+    }
+    if (value > root.value) {
+      // go right
+      // eslint-disable-next-line no-param-reassign
+      root.rightChild = this.insert(root.rightChild, value);
+      return root;
+    }
+    return root;
+  }
+}
+
+// const bst = new BinarySearchTree(10);
+// console.log(bst.root);
+// bst.insert(bst.root, 12);
+// bst.insert(bst.root, 9);
+// bst.insert(bst.root, 19);
+// bst.insert(bst.root, 11);
+// bst.insert(bst.root, 6);
+
+// console.log(bst.root);
+
+module.exports = BinarySearchTree;
