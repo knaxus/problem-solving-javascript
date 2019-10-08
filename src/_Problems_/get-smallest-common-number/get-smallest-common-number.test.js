886e4fd24139008691abd335402cb4afac80851f
Darwin Cahyadi <darwin.cahyadi@ninjavan.co>
2019-10-08 17:46:18 +0700

    add test smallest number

---
+++ src/_Problems_/get-smallest-common-number/get-smallest-common-number.test.js
@@ -0,0 +1,24 @@
+const { getSmallestCommonNumber } = require('.');
+
+describe('Get common smallest number between two integer arrays', () => {
+  it('Should return -1 when both has empty array', () => {
+    const arr1 = [];
+    const arr2 = [];
+
+    expect(getSmallestCommonNumber(arr1, arr2)).toEqual(-1);
+  });
+
+  it('Should return -1 when no common between two integer arrays', () => {
+    const arr1 = [1, 3, 5];
+    const arr2 = [2, 4, 6];
+
+    expect(getSmallestCommonNumber(arr1, arr2)).toEqual(-1);
+  });
+
+  it('Should return common smallest number between two integer arrays', () => {
+    const arr1 = [2, 3];
+    const arr2 = [2, 5, 7];
+
+    expect(getSmallestCommonNumber(arr1, arr2)).toEqual(2);
+  });
+});
