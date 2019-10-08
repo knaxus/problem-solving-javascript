cdfa983be7ff8e1857602702b4428c1dfbcaf5a6
OMKAR AGRAWAL <Omkaragrawal@users.noreply.github.com>
2019-10-09 01:26:05 +0530

    tests added

--- src/_Classics_/caeser_cipher/caesarCipher.test.js
+++ src/_Classics_/caeser_cipher/caesarCipher.test.js
@@ -13,7 +13,55 @@ const compareString = (str1, str2) => {
 describe('Caesar Cipher tests', () => {
   it('Shift not passed', () => {
     const ocString = 'Hello World';
-    const enciphered = caesarCipher(ocString);
-    expect(compareString(ocString, enciphered)).toBe(true);
+    const enCiphered = caesarCipher(ocString);
+    expect(compareString(ocString, enCiphered)).toBe(true);
+  });
+
+  it('Encipher equals decipher', () => {
+    const ocString = 'Hello World';
+    const enCiphered = caesarCipher(ocString, 1);
+    const deCiphered = caesarCipher(enCiphered, -1);
+    expect(compareString(ocString, deCiphered)).toBe(true);
+  });
+
+  it('Shift lower than string length works', () => {
+    const ocString = 'Hello World';
+    const enCiphered = caesarCipher(ocString, 1);
+    const idealResult = 'Ifmmp Xpsme';
+    expect(compareString(enCiphered, idealResult)).toBe(true);
+  });
+
+  it('Shift Higher than string length works', () => {
+    const ocString = 'Hello World';
+    const enCiphered = caesarCipher(ocString, 63);
+    const idealResult = caesarCipher(ocString, 1);
+    expect(compareString(enCiphered, idealResult)).toBe(true);
+  });
+
+  it('Negative shift Works', () => {
+    const ocString = 'Ifmmp Xpsme';
+    const enCiphered = caesarCipher(ocString, -1);
+    const idealResult = 'Hello World';
+    expect(compareString(enCiphered, idealResult)).toBe(true);
+  });
+
+  it('Shift is a numerical string', () => {
+    const ocString = 'Hello World';
+    const enCiphered = caesarCipher(ocString, '1');
+    const idealResult = 'Ifmmp Xpsme';
+    expect(compareString(enCiphered, idealResult)).toBe(true);
+    // toThrowError
+  });
+
+  it('Shift is not a numbered string', () => {
+    expect(caesarCipher('Hello World', 'abc')).toThrowError('Invalid Shift Provided');
+  });
+
+  it('toEncipher is NaN', () => {
+    expect(caesarCipher(NaN, 1)).toThrowError('Invalid string provided');
+  });
+
+  it('toEncipher is undefined', () => {
+    expect(caesarCipher(undefined, 1)).toThrowError('Invalid string provided');
   });
 });
