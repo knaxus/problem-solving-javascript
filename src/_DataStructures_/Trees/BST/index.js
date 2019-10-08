b62984b25565d425991089dc75a448851ab97e66
Ashok Dey <ashokdey100@gmail.com>
2019-10-09 01:59:04 +0530

    cleanup

--- src/_DataStructures_/Trees/BST/index.js
+++ src/_DataStructures_/Trees/BST/index.js
@@ -157,44 +157,35 @@ class BinarySearchTree {
   }
 }
 
-const bst = new BinarySearchTree(6);
-console.log(bst.root);
-bst.add(4);
-bst.add(9);
-bst.add(2);
-bst.add(5);
-bst.add(8);
-bst.add(12);
+// const bst = new BinarySearchTree(6);
+// console.log(bst.root);
+// bst.add(4);
+// bst.add(9);
+// bst.add(2);
+// bst.add(5);
+// bst.add(8);
+// bst.add(12);
 
-console.log(bst.root);
+// console.log(bst.root);
 
-const preorder = bst.traversePreorder();
-console.log('Preorder Traversal - ', preorder);
+// const preorder = bst.traversePreorder();
+// console.log('Preorder Traversal - ', preorder);
 
-const inorder = bst.traverseInorder();
-console.log('Inorder Traversal - ', inorder);
+// const inorder = bst.traverseInorder();
+// console.log('Inorder Traversal - ', inorder);
 
-const postorder = bst.traversePostorder();
-console.log('Postorder Traversal - ', postorder);
+// const postorder = bst.traversePostorder();
+// console.log('Postorder Traversal - ', postorder);
 
-const search = 18;
-console.log(`Search for ${search}`, bst.searchFor(search));
+// const search = 18;
+// console.log(`Search for ${search}`, bst.searchFor(search));
 
-const minNode = bst.findMinimum();
-console.log('Minimum value =>', minNode);
+// const minNode = bst.findMinimum();
+// console.log('Minimum value =>', minNode);
 
-bst.remove(4);
-console.log(bst.traversePreorder());
-
-console.log(bst.root);
-
-// bst.remove(2);
+// bst.remove(4);
 // console.log(bst.traversePreorder());
 
-// bst.remove(9);
-// console.log(bst.traversePreorder());
-
-// bst.remove(12);
-// console.log(bst.traversePreorder());
+// console.log(bst.root);
 
 module.exports = BinarySearchTree;
