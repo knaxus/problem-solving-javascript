e8e30b7747c570d8745d050157c35e7d8b0e6158
OMKAR AGRAWAL <Omkaragrawal@users.noreply.github.com>
2019-10-10 01:45:22 +0530

    index.js removed and indentations fixed to eslint.

--- src/_Classics_/caesar_cipher/caeserCipher.js
+++ src/_Classics_/caesar_cipher/caeserCipher.js
@@ -1,43 +1,46 @@
+/**
+ * Most simplest encryption scheme. Read more: [http://practicalcryptography.com/ciphers/caesar-cipher/]
+ **/
 function caesarCipher(toEncipher, shift = 0) {
-    // If required for very strict shift checking then remove '=0'
-    if (Number.isNaN(Number(shift)) === true) {
-    throw new Error('Invalid Shift Provided')
-    } else {
-        shift = Number(shift);
-    }
+  // If required for very strict shift checking then remove '=0'
+  if (Number.isNaN(Number(shift)) === true) {
+    throw new Error('Invalid Shift Provided');
+  } else {
+    shift = Number(shift);
+  }
 
-    if(typeof(toEncipher) === 'string' || (typeof(toEncipher) === 'number' && Number.isNaN(toEncipher) === false)) {
-        toEncipher = String(toEncipher);
-    } else {
-        throw new Error('Invalid string provided');
-    }
+  if (typeof (toEncipher) === 'string' || (typeof (toEncipher) === 'number' && Number.isNaN(toEncipher) === false)) {
+    toEncipher = String(toEncipher);
+  } else {
+    throw new Error('Invalid string provided');
+  }
 
-    // These are the valid entries aacepted, you can change it according to requirements
-    const validEntries = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  // These are the valid entries aacepted, you can change it according to requirements
+  const validEntries = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
 
-    shift %= validEntries.length;
+  shift %= validEntries.length;
 
-    let output = '';
-    for (let char of toEncipher) {
-        if (char === ' ') {
-            output += ' ';
-            continue;
-    //------- If you donot want to accept invalid entries then uncomment below block
+  let output = '';
+  for (const char of toEncipher) {
+    if (char === ' ') {
+      output += ' ';
+      continue;
+      // ------- If you donot want to accept invalid entries then uncomment below block
     /*
     ========================================================================================================
             } else if (validEntries.indexOf(char) === -1) {return Error ('invalid character' + char)}
     ========================================================================================================
     and comment out the below block
     */
-   //=======================================================================================================
-        } else if (validEntries.indexOf(char) === -1) {
-            output += char;
-            continue;
-        }
-   //=======================================================================================================
-   output += (validEntries.indexOf(char) + shift <= validEntries.length) ? validEntries[validEntries.indexOf(char) + shift] : validEntries[(validEntries.indexOf(char) + shift) % validEntries.length];
+      //= ======================================================================================================
+    } else if (validEntries.indexOf(char) === -1) {
+      output += char;
+      continue;
     }
-    return output;
+    //= ======================================================================================================
+    output += (validEntries.indexOf(char) + shift <= validEntries.length) ? validEntries[validEntries.indexOf(char) + shift] : validEntries[(validEntries.indexOf(char) + shift) % validEntries.length];
+  }
+  return output;
 }
 
 module.exports = caesarCipher;
