6b3d07a001ec3d1e8ff7e48fd3cbfb8b3c3df82c
OMKAR AGRAWAL <Omkaragrawal@users.noreply.github.com>
2019-10-12 14:07:25 +0530

    all the comparisions in caesarCipher.test repaired

--- src/_Classics_/caesar_cipher/caesarCipher.test.js
+++ src/_Classics_/caesar_cipher/caesarCipher.test.js
@@ -2,33 +2,25 @@
 /* eslint-disable no-else-return */
 const caesarCipher = require('./caeserCipher');
 
-const compareString = (str1, str2) => {
-  if (str1 === str2) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
 describe('Caesar Cipher tests', () => {
   it('Shift not passed', () => {
     const ocString = 'Hello World';
     const enCiphered = caesarCipher(ocString);
-    expect(compareString(ocString, enCiphered)).toBe(true);
+    expect(ocString).toBe(enCiphered);
   });
 
   it('Encipher equals decipher', () => {
     const ocString = 'Hello World';
     const enCiphered = caesarCipher(ocString, 1);
     const deCiphered = caesarCipher(enCiphered, -1);
-    expect(compareString(ocString, deCiphered)).toBe(true);
+    expect(ocString).toBe(deCiphered);
   });
 
   it('Shift lower than string length works', () => {
     const ocString = 'Hello World';
     const enCiphered = caesarCipher(ocString, 1);
     const idealResult = 'Ifmmp Xpsme';
-    expect(compareString(enCiphered, idealResult)).toBe(true);
+    expect(enCiphered).toBe(idealResult);
   });
 
   it('Shift Higher than string length works', () => {
@@ -42,15 +34,14 @@ describe('Caesar Cipher tests', () => {
     const ocString = 'Ifmmp Xpsme';
     const enCiphered = caesarCipher(ocString, -1);
     const idealResult = 'Hello World';
-    expect(compareString(enCiphered, idealResult)).toBe(true);
+    expect(enCiphered).toBe(idealResult);
   });
 
   it('Shift is a numerical string', () => {
     const ocString = 'Hello World';
     const enCiphered = caesarCipher(ocString, '1');
     const idealResult = 'Ifmmp Xpsme';
-    expect(compareString(enCiphered, idealResult)).toBe(true);
-    // toThrowError
+    expect(enCiphered).toBe(idealResult);
   });
 
   it('Shift is not a numbered string', () => {
