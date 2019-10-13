ea84561d7278680c449e891faf35e6ed33cc5ec2
Ashok Dey <ashokdey100@gmail.com>
2019-10-13 13:24:05 +0530

    fix: peek() should show the front item of queue

--- src/_DataStructures_/Queue/Queue.test.js
+++ src/_DataStructures_/Queue/Queue.test.js
@@ -12,23 +12,24 @@ describe('Data Structure : Queue', () => {
       queue = new Queue();
     });
 
-    it('Should add() element to a queue', () => {
-      queue.add(5);
-      expect(queue.data).toEqual([5]);
+    it('Should enqueue() element to a queue', () => {
+      queue.enqueue(5);
+      expect(queue.peek()).toEqual([5]);
     });
 
-    it('Should remove() an element from the queue', () => {
-      queue.add(2);
-      queue.add(3);
+    it('Should dequeue() an element from the queue', () => {
+      queue.enqueue(2);
+      queue.enqueue(3);
 
-      expect(queue.remove()).toEqual(2);
-      expect(queue.data).toEqual([3]);
+      expect(queue.dequeue()).toEqual(2);
+      expect(queue.peek()).toEqual(3);
+      expect(queue.size()).toEqual(1);
     });
 
     describe('peek()', () => {
       beforeEach(() => {
-        queue.add(2);
-        queue.add(5);
+        queue.enqueue(2);
+        queue.enqueue(5);
       });
 
       it('Should return the elemet to be removed using peek()', () => {
