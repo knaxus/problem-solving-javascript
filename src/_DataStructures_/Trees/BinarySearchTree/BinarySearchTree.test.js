2c354b8fda46cd8b9cbe5c15f2dfccc321c1cded
SHEEVOOT <sheevoot@apples-MacBook-Pro.local>
2019-10-14 10:18:22 +0530

    delete test removed

--- src/_DataStructures_/Trees/BinarySearchTree/BinarySearchTree.test.js
+++ src/_DataStructures_/Trees/BinarySearchTree/BinarySearchTree.test.js
@@ -21,15 +21,5 @@ describe('Data Structure : Binary Search Tree', () => {
             bst.insert(bst.root, 7);
             expect(bst.traverseInorder()).toEqual([2, 3, 4, 5, 7, 9]);
         });
-
-        it('Should delete() an element from Binary Search Tree', () => {
-            bst.add(4);
-            bst.add(9);
-            bst.add(2);
-            bst.delete(bst.root, 4);
-            expect(bst.traverseInorder()).toEqual([2, 5, 9]);
-            bst.delete(bst.root, 2);
-            expect(bst.traverseInorder()).toEqual([5, 9]);
-        });
     });
 });
