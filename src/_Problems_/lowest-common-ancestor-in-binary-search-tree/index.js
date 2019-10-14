d0e76c8a339108f887626531ae83300fd3b7079b
A.Mahdy AbdelAziz <amahdy@users.noreply.github.com>
2019-10-15 01:02:38 +0200

    Add the algorithm
    Ref: https://www.geeksforgeeks.org/lowest-common-ancestor-in-a-binary-search-tree/

--- src/_Problems_/lowest-common-ancestor-in-binary-search-tree/index.js
+++ src/_Problems_/lowest-common-ancestor-in-binary-search-tree/index.js
@@ -4,4 +4,21 @@
  * Given values of two values n1 and n2 in a Binary Search Tree, find the Lowest Common Ancestor (LCA). You may assume that both the values exist in the tree.
  */
  
- 
+function lca(node, n1, n2) {
+  if (node == null)
+    return null;
+
+  // If both n1 and n2 are smaller than root, then LCA lies in left
+  if (node.data > n1 && node.data > n2)
+    return lca(node.left, n1, n2);
+
+  // If both n1 and n2 are greater than root, then LCA lies in right
+  if (node.data < n1 && node.data < n2)
+    return lca(node.right, n1, n2);
+
+  return node;
+}
+
+module.exports = {
+  lca,
+};
