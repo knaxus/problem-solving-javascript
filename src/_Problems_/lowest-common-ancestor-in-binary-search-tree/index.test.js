bd37bb08b8ef870efd40f336b9fe6ea1888548c0
A.Mahdy AbdelAziz <amahdy@users.noreply.github.com>
2019-10-15 01:37:25 +0200

    Fix test by adding missing arguments

--- src/_Problems_/lowest-common-ancestor-in-binary-search-tree/index.test.js
+++ src/_Problems_/lowest-common-ancestor-in-binary-search-tree/index.test.js
@@ -2,7 +2,12 @@ const { lca } = require('.');
 
 describe('LCA', () => {
   it('Should return 12', () => {
-    expect(lca({"left":{"left":{"data":4},"right":{"left":{"data":10},"right":{"data":14},"data":12},"data":8},"right":{"data":22},"data":20}).data).toEqual(12);
+    expect(
+      lca(
+        {"left":{"left":{"data":4},"right":{"left":{"data":10},"right":{"data":14},"data":12},"data":8},"right":{"data":22},"data":20}
+        , 10
+        , 14)
+      .data).toEqual(12);
   });
 });
 
