a28632750bc86d3b1246a6e4f4e259dfee0a0e9d
A.Mahdy AbdelAziz <amahdy@users.noreply.github.com>
2019-10-15 00:33:03 +0200

    Create solution file

---
+++ src/_Searching_/LowestCommonAncestor/index.js
@@ -0,0 +1,7 @@
+/**
+ * Lowest Common Ancestor in a Binary Search Tree.
+ *
+ * Given values of two values n1 and n2 in a Binary Search Tree, find the Lowest Common Ancestor (LCA). You may assume that both the values exist in the tree.
+ */
+ 
+ 
