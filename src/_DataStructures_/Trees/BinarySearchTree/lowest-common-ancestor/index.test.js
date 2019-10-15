5a83abbe57664657b3a3c714032cac01af9c72f3
A.Mahdy AbdelAziz <amahdy@users.noreply.github.com>
2019-10-15 19:33:19 +0200

    Fix variable declaration

--- src/_DataStructures_/Trees/BinarySearchTree/lowest-common-ancestor/index.test.js
+++ src/_DataStructures_/Trees/BinarySearchTree/lowest-common-ancestor/index.test.js
@@ -1,18 +1,19 @@
 const { lca } = require('.');
-const { BinarySearchTree } = require('..');
-
-const bst = new BinarySearchTree(20);
-bst.add(22);
-bst.add(8);
-bst.add(12);
-bst.add(4);
-bst.add(14);
-bst.add(10);
+const BinarySearchTree = require('../index');
 
 // Quick JSON equivalent
 // {"left":{"left":{"data":4},"right":{"left":{"data":10},"right":{"data":14},"data":12},"data":8},"right":{"data":22},"data":20}
 
 describe('LCA', () => {
+
+  let bst = new BinarySearchTree(20);
+  bst.add(22);
+  bst.add(8);
+  bst.add(12);
+  bst.add(4);
+  bst.add(14);
+  bst.add(10);
+
   it('Should return 12', () => {
     expect(lca(bst.root, 10, 14).data).toEqual(12);
   });
