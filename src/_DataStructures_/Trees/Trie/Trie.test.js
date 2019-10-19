7d0fa877fc7a237a213e090ad4cb1474637130d3
Reshmi <reshmisaji50@gmail.com>
2019-10-19 20:50:04 +0530

    Add some more Test For - Trie search string #106

--- src/_DataStructures_/Trees/Trie/Trie.test.js
+++ src/_DataStructures_/Trees/Trie/Trie.test.js
@@ -21,6 +21,13 @@ describe('Data Structure : Trie', () => {
       expect(result).toEqual(false);
     });
 
+    it('Should return true when insert is called with a key', () => {
+      const result = newTrie.insert('dflkghik');
+      const newResult = newTrie.insert('Lorem Ipsum');
+      expect(result).toEqual(true);
+      expect(newResult).toEqual(true);
+    });
+
     it('Should return false when search is called without a key', () => {
       const result = newTrie.search();
       expect(result).toEqual(false);
@@ -34,13 +41,21 @@ describe('Data Structure : Trie', () => {
     it('Should return true when searched for a key which present in trie', () => {
       const value = 'abc';
       newTrie.insert(value);
+      newTrie.insert('dflkghik');
+      newTrie.insert('Lorem Ipsum');
       const result = newTrie.search(value);
+      const newResult = newTrie.search('dflkghik');
+      const loremIpsumResult = newTrie.search('Lorem Ipsum');
       expect(result).toEqual(true);
+      expect(newResult).toEqual(true);
+      expect(loremIpsumResult).toEqual(true);
     });
 
     it('Should return the index of the Character', () => {
       const result = newTrie.getIndexOfChar('a');
+      const newResult = newTrie.getIndexOfChar('A');
       expect(result).toEqual(0);
+      expect(newResult).toEqual(-32);
     });
   });
 });
