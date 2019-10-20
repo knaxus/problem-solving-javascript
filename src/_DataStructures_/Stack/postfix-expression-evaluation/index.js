6135d56992eb60a05584aadd24b8df18b1e83e29
Manish Kumar <manishkumar@XE-GGN-IT-01985.local>
2019-10-20 15:44:01 +0530

    Revert changes for #61

--- src/_DataStructures_/Stack/postfix-expression-evaluation/index.js
+++ src/_DataStructures_/Stack/postfix-expression-evaluation/index.js
@@ -6,56 +6,39 @@
 
 const Stack = require('../index');
 
-const ERROR_STRING = 'Expression is not in order';
-
 function evaluatePostfixExpression(expression) {
-  // eslint-disable-next-line no-param-reassign
-  expression = expression.trim();
+    let s = new Stack();
+    for (let i = 0; i < expression.length; i++) {
+        const char = expression[i];
+        if (!isNaN(char)) {
+            //if number push the char onto stack
+            s.push(Number(char));
+        } else {
+            // if char is an operator then pop two elements from stack, evaluate them accordingly based on operator.
+            //push the result to stack
+            let val1 = s.pop();
+            let val2 = s.pop();
+            switch (char) {
+                case '+':
+                    s.push(val2 + val1);
+                    break;
+                case '-':
+                    s.push(val2 - val1);
+                    break;
+                case '*':
+                    s.push(val2 * val1);
+                    break;
+                case '/':
+                    s.push(val2 / val1);
+                    break;
 
-  if (expression.length === 0 || expression.length === 1) {
-    return ERROR_STRING;
-  }
-
-  const s = new Stack();
-  // eslint-disable-next-line no-plusplus
-  for (let i = 0; i < expression.length; i++) {
-    const char = expression[i];
-    // eslint-disable-next-line no-restricted-globals
-    if (!isNaN(char)) {
-      // if number push the char onto stack
-      s.push(Number(char));
-    } else {
-      // if char is an operator then pop two elements from stack, evaluate them accordingly based on operator.
-      // push the result to stack
-      const val1 = s.pop();
-      const val2 = s.pop();
-      switch (char) {
-        case '+':
-          s.push(val2 + val1);
-          break;
-        case '-':
-          s.push(val2 - val1);
-          break;
-        case '*':
-          s.push(val2 * val1);
-          break;
-        case '/':
-          s.push(val2 / val1);
-          break;
-        default:
-          break;
-      }
+            }
+        }
     }
-  }
-  // pop the value of postfix expression
-  const result = s.pop();
-  if (s.isEmpty()) {
-    return result;
-  }
-  return ERROR_STRING;
+    //pop the value of postfix expression
+    return s.pop();
 }
 
 module.exports = {
   evaluatePostfixExpression,
-  ERROR_STRING
 };
--- src/_DataStructures_/Stack/postfix-expression-evaluation/postfix-expression-evaluation.test.js
+++ src/_DataStructures_/Stack/postfix-expression-evaluation/postfix-expression-evaluation.test.js
@@ -1,63 +1,55 @@
-const { evaluatePostfixExpression, ERROR_STRING } = require('.');
+const { evaluatePostfixExpression } = require('.');
 
-describe('Postfix expression evaluation', () => {
-  it('should be a function', () => {
+describe('Postfix expression evaluation', function () {
+  it('should be a function', function () {
     expect(typeof evaluatePostfixExpression).toEqual('function');
   });
-
-  it('should return a number', () => {
+  
+  it('should return a number', function () {
     const expression = '11+';
-
-    expect(typeof evaluatePostfixExpression(expression)).toEqual('number');
+    
+    expect(typeof evaluatePostfixExpression(expression)).toEqual('number')
   });
-
-  it('should handle addition', () => {
+  
+  it('should handle addition', function () {
     const expression = '23+';
     const expected = 5;
-
+    
     expect(evaluatePostfixExpression(expression)).toEqual(expected);
   });
-
-  it('should handle subtraction', () => {
+  
+  it('should handle subtraction', function () {
     const expression = '54-';
     const expected = 1;
-
+    
     expect(evaluatePostfixExpression(expression)).toEqual(expected);
   });
-
-  it('should handle multiplication', () => {
+  
+  it('should handle multiplication', function () {
     const expression = '34*';
     const expected = 12;
-
+    
     expect(evaluatePostfixExpression(expression)).toEqual(expected);
   });
-
-  it('should handle division', () => {
+  
+  it('should handle division', function () {
     const expression = '62/';
     const expected = 3;
-
+    
     expect(evaluatePostfixExpression(expression)).toEqual(expected);
   });
-
-  it('should handle negative numbers', () => {
+  
+  it('should handle negative numbers', function () {
     const expression = '25-';
     const expected = -3;
-
+    
     expect(evaluatePostfixExpression(expression)).toEqual(expected);
   });
-
-  it('should handle multiple operators', () => {
+  
+  it('should handle multiple operators', function () {
     const expression = '123*+';
     const expected = 7;
-
+    
     expect(evaluatePostfixExpression(expression)).toEqual(expected);
-  });
-
-  describe('should throw error on invalid expressions', () => {
-    const invalidExpressions = ['12', '1', '+', '1+2', '+12'];
-    test.each(invalidExpressions)('running for %p', expression => {
-      const result = evaluatePostfixExpression(expression);
-      expect(result).toEqual(ERROR_STRING);
-    });
   });
 });
