67cf7c082ea12d62361fc29e6e579a4580db585c
Manish Kumar <manishkumar@XE-GGN-IT-01985.local>
2019-10-20 15:37:12 +0530

    #61 - postfix-expression-evaluation
    - Fixed ESlint errors
    - throw error for invalid string

--- src/_DataStructures_/Stack/postfix-expression-evaluation/postfix-expression-evaluation.test.js
+++ src/_DataStructures_/Stack/postfix-expression-evaluation/postfix-expression-evaluation.test.js
@@ -1,55 +1,63 @@
-const { evaluatePostfixExpression } = require('.');
+const { evaluatePostfixExpression, ERROR_STRING } = require('.');
 
-describe('Postfix expression evaluation', function () {
-  it('should be a function', function () {
+describe('Postfix expression evaluation', () => {
+  it('should be a function', () => {
     expect(typeof evaluatePostfixExpression).toEqual('function');
   });
-  
-  it('should return a number', function () {
+
+  it('should return a number', () => {
     const expression = '11+';
-    
-    expect(typeof evaluatePostfixExpression(expression)).toEqual('number')
+
+    expect(typeof evaluatePostfixExpression(expression)).toEqual('number');
   });
-  
-  it('should handle addition', function () {
+
+  it('should handle addition', () => {
     const expression = '23+';
     const expected = 5;
-    
+
     expect(evaluatePostfixExpression(expression)).toEqual(expected);
   });
-  
-  it('should handle subtraction', function () {
+
+  it('should handle subtraction', () => {
     const expression = '54-';
     const expected = 1;
-    
+
     expect(evaluatePostfixExpression(expression)).toEqual(expected);
   });
-  
-  it('should handle multiplication', function () {
+
+  it('should handle multiplication', () => {
     const expression = '34*';
     const expected = 12;
-    
+
     expect(evaluatePostfixExpression(expression)).toEqual(expected);
   });
-  
-  it('should handle division', function () {
+
+  it('should handle division', () => {
     const expression = '62/';
     const expected = 3;
-    
+
     expect(evaluatePostfixExpression(expression)).toEqual(expected);
   });
-  
-  it('should handle negative numbers', function () {
+
+  it('should handle negative numbers', () => {
     const expression = '25-';
     const expected = -3;
-    
+
     expect(evaluatePostfixExpression(expression)).toEqual(expected);
   });
-  
-  it('should handle multiple operators', function () {
+
+  it('should handle multiple operators', () => {
     const expression = '123*+';
     const expected = 7;
-    
+
     expect(evaluatePostfixExpression(expression)).toEqual(expected);
+  });
+
+  describe('should throw error on invalid expressions', () => {
+    const invalidExpressions = ['12', '1', '+', '1+2', '+12'];
+    test.each(invalidExpressions)('running for %p', expression => {
+      const result = evaluatePostfixExpression(expression);
+      expect(result).toEqual(ERROR_STRING);
+    });
   });
 });
