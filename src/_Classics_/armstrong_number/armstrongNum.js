19c4df1ab28e4ba254c7fcd513e668fdf052e872
OMKAR AGRAWAL <Omkaragrawal@users.noreply.github.com>
2019-10-21 07:49:09 +0530

    Created armstrong number
    added armstrongNum.js

---
+++ src/_Classics_/armstrong_number/armstrongNum.js
@@ -0,0 +1,12 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-param-reassign */
+
+function armstrongNum(num) {
+  num = (!Number.isNaN(Number(num))) ? parseInt(Math.abs(Number(num)), 10).toString() : (() => { throw Error('error'); })();
+  let total = 0;
+  for (let i = 0; i < num.length; i += 1) {
+    total += num[i] ** num.length;
+  }
+  return (total === Math.abs(Number(num))) ? true : false;
+}
+module.exports = armstrongNum;
