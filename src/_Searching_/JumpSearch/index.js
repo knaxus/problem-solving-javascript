ab84445ae992864734c7f2d433fb1f7a18caf492
ashudeshwal999 <ashudeshwal999@gmail.com>
2019-10-25 15:59:59 +0530

    --fix : eslint & coverage 100% & return -1 if not found

--- src/_Searching_/JumpSearch/index.js
+++ src/_Searching_/JumpSearch/index.js
@@ -1,38 +1,32 @@
 /**
  * Note: Array must be sorted for jump search
- * Complexity: 
+ * Complexity:
  * Worst case time complexity: O(√N)
  * Average case time complexity: O(√N)
  * Best case time complexity: O(1)
  * Space complexity: O(1)
 */
 function jumpSearch(arr, key) {
-    const n = arr.length;
-    const jump = Math.floor(Math.sqrt(n));
-    let step = jump;
+  const n = arr.length;
+  const jump = Math.floor(Math.sqrt(n));
+  let step = jump;
+  let prev = 0;
 
-    let prev = 0;
-    
-    while(arr[Math.min(step, n) - 1] < key) {
-        prev = step;
-        step += jump;
-        if (prev >= n) 
-            return null; 
-    }
-
-    while(arr[prev] < key) {
-        prev++;
-
-        if (prev == Math.min(step, n))
-            return null;
-    }
-    
-    if (arr[prev] == key) 
-        return prev; 
-
-    return null;
+  while (arr[Math.min(step, n) - 1] < key) {
+    prev = step;
+    step += jump;
+    if (prev >= n) { return -1; }
   }
-  
-  module.exports = {
-    jumpSearch,
-  };
+
+  while (arr[prev] < key && prev < Math.min(step, n)) {
+    prev += 1;
+  }
+
+  if (arr[prev] === key) { return prev; }
+
+  return -1;
+}
+
+module.exports = {
+  jumpSearch,
+};
