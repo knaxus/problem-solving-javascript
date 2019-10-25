6617507a6176bd916cd8ce8e78ecdd40c785edad
Christian Fei <crifei93@gmail.com>
2019-10-26 00:17:09 +0200

    add test for bottom view binary tree

---
+++ src/_DataStructures_/Trees/BinaryTree/bottom-view-binary-tree/BottomViewBinaryTree.test.js
@@ -0,0 +1,14 @@
+const BinaryTree = require("../index");
+const bottomView = require('.');
+
+describe('Bottom View Binary Tree', () => {
+  let btree
+
+  beforeEach(() => {
+    btree = new BinaryTree([1, 2, 3, 4, 5, 6]);
+  });
+
+  it('Determines the bottom view of a binary tree', () => {
+    expect(bottomView(btree)).toEqual([6, 2, 3, 4]);
+  });
+});
