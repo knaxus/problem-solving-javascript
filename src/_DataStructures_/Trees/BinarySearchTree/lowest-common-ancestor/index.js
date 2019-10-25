9840f5e45158f3b955e529289585e87d2b88f103
ashudeshwal999 <ashudeshwal999@gmail.com>
2019-10-25 21:25:36 +0530

    --fix : eslint & code coverage 100%

--- src/_DataStructures_/Trees/BinarySearchTree/lowest-common-ancestor/index.js
+++ src/_DataStructures_/Trees/BinarySearchTree/lowest-common-ancestor/index.js
@@ -3,18 +3,15 @@
  *
  * Given values of two values n1 and n2 in a Binary Search Tree, find the Lowest Common Ancestor (LCA). You may assume that both the values exist in the tree.
  */
- 
+
 function lca(node, n1, n2) {
-  if (node == null)
-    return null;
+  if (node == null) return null;
 
   // If both n1 and n2 are smaller than root, then LCA lies in left
-  if (node.value > n1 && node.value > n2)
-    return lca(node.leftChild, n1, n2);
+  if (node.value > n1 && node.value > n2) { return lca(node.leftChild, n1, n2); }
 
   // If both n1 and n2 are greater than root, then LCA lies in right
-  if (node.value < n1 && node.value < n2)
-    return lca(node.rightChild, n1, n2);
+  if (node.value < n1 && node.value < n2) { return lca(node.rightChild, n1, n2); }
 
   return node;
 }
--- src/_DataStructures_/Trees/BinarySearchTree/lowest-common-ancestor/index.test.js
+++ src/_DataStructures_/Trees/BinarySearchTree/lowest-common-ancestor/index.test.js
@@ -5,8 +5,7 @@ const BinarySearchTree = require('../index');
 // {"left":{"left":{"data":4},"right":{"left":{"data":10},"right":{"data":14},"data":12},"data":8},"right":{"data":22},"data":20}
 
 describe('LCA', () => {
-
-  let bst = new BinarySearchTree(20);
+  const bst = new BinarySearchTree(20);
   bst.add(22);
   bst.add(8);
   bst.add(12);
@@ -17,12 +16,19 @@ describe('LCA', () => {
   it('Should return 12', () => {
     expect(lca(bst.root, 10, 14).value).toEqual(12);
   });
-  
+
   it('Should return 8', () => {
     expect(lca(bst.root, 14, 8).value).toEqual(8);
   });
-  
+
   it('Should return 20', () => {
     expect(lca(bst.root, 10, 22).value).toEqual(20);
+  });
+
+  const bst2 = new BinarySearchTree(6);
+  bst2.remove(6);
+
+  it('Should return Null', () => {
+    expect(lca(bst2.root, 10, 22)).toEqual(null);
   });
 });
