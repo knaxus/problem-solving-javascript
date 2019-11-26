edb379a9597ec629e9a5338443ee31d2ab638b98
ashudeshwal999 <ashudeshwal999@gmail.com>
2019-11-26 23:56:17 +0530

    --fix: code coverage to 100%

--- src/_Problems_/binary-tree-to-binary-search-tree/binary-tree-to-binary-search-tree.test.js
+++ src/_Problems_/binary-tree-to-binary-search-tree/binary-tree-to-binary-search-tree.test.js
@@ -4,11 +4,14 @@ const BinaryTree = require('../../_DataStructures_/Trees/BinaryTree');
 describe('Binary tree to binary search tree', () => {
   let tree;
 
-  describe('Create Binary Tree', () => {
-    tree = new BinaryTree([10, 30, 15, 20, null, null, 5]);
+  it('Should return `null` if root is null', () => {
+    tree = new BinaryTree([1]);
+    tree.root = null;
+    expect(binaryTreeToBST(tree)).toEqual(null);
   });
 
   it('Should converted binary tree to binary search tree', () => {
+    tree = new BinaryTree([10, 30, 15, 20, null, null, 5]);
     const bTree = binaryTreeToBST(tree);
     expect(storeInorder(bTree)).toEqual([5, 10, 15, 20, 30]);
   });
--- src/_Problems_/next-greater-element/next-greater-element.test.js
+++ src/_Problems_/next-greater-element/next-greater-element.test.js
@@ -1,38 +1,38 @@
 const { nextGreaterElement } = require('.');
 
 describe('Next greater element', () => {
-  it('returns next greater elements collection', () => {
-    const input = [4, 6, 3, 2, 8, 1]
-    const greaterElements = [6, 8, 8, 8, -1, -1]
+  it('Should returns next greater elements collection', () => {
+    const input = [4, 11, 6, 3, 2, 8, 1];
+    const greaterElements = [11, -1, 8, 8, 8, -1, -1];
 
     expect(nextGreaterElement(input)).toEqual(greaterElements);
   });
 
-  it('returns and empty collection for an empty array', () => {
+  it('Should returns and empty collection for an empty array', () => {
     expect(nextGreaterElement([])).toEqual([]);
   });
 
-  it('returns an array with -1 if the input has only one element', () => {
+  it('Should returns an array with -1 if the input has only one element', () => {
     expect(nextGreaterElement([0])).toEqual([-1]);
   });
 
-  it('returns a collection of -1 if there is no greater element', () => {
-    const input = [90, 40, 15, 7, -1, -10]
-    const greaterElements = [-1, -1, -1, -1, -1, -1]
+  it('Should returns a collection of -1 if there is no greater element', () => {
+    const input = [90, 40, 15, 7, -1, -10];
+    const greaterElements = [-1, -1, -1, -1, -1, -1];
 
     expect(nextGreaterElement(input)).toEqual(greaterElements);
   });
 
-  it('uses -1 if the numbers are the same', () => {
-    const input = [90, 90]
-    const greaterElements = [-1, -1]
+  it('Should uses -1 if the numbers are the same', () => {
+    const input = [90, 90];
+    const greaterElements = [-1, -1];
 
     expect(nextGreaterElement(input)).toEqual(greaterElements);
   });
 
-  it('throws an error if the input is not an array', () => {
+  it('Should throws an error if the input is not an array', () => {
     expect(() => {
-      nextGreaterElement('xunda')
+      nextGreaterElement('xunda');
     }).toThrowError('Invalid Argument');
   });
 });
