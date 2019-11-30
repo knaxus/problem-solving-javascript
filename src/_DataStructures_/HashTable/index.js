ae5ea995e5a98dca0987974357364ca72b147d83
Ashok Dey <ashokdey100@gmail.com>
2019-11-30 11:46:07 +0530

    update: added rehashing to avoid collision

--- src/_DataStructures_/HashTable/index.js
+++ src/_DataStructures_/HashTable/index.js
@@ -3,10 +3,12 @@ const HashEntry = require('./HashEntry');
 class HashTable {
   constructor(slots) {
     // init with a default set of slots
-    this.slot = slots || 17;
+    this.slot = slots || 19;
     // size to hold the current size
     // and help to resize when the table is half filled
     this.size = 0;
+    // threshold (let it be 70%)
+    this.threshold = 0.7;
     // the main bucket
     this.bucket = new Array(this.slot);
 
@@ -30,6 +32,34 @@ class HashTable {
       index = (index + PRIME + value) % this.bucket.length;
     }
     return index;
+  }
+
+  _resize() {
+    const oldSlot = this.slot;
+    const oldBucket = this.bucket;
+
+    this.slot = oldSlot * 2;
+    const newBucket = new Array(this.slot);
+    // fill the new bucket with nulls
+    for (let i = 0; i < this.slot; i += 1) newBucket[i] = null;
+
+    this.bucket = newBucket;
+
+    for (let i = 0; i < oldSlot; i += 1) {
+      if (oldBucket[i]) {
+        // get all the nodes associated here
+        let head = oldBucket[i];
+
+        while (head !== null) {
+          const { key, value } = head;
+          // eslint-disable-next-line no-underscore-dangle
+          const newIndex = this._hash(key);
+          // eslint-disable-next-line no-underscore-dangle
+          this._push(newIndex, { key, value });
+          head = head.next;
+        }
+      }
+    }
   }
 
   _push(index, value) {
@@ -76,6 +106,17 @@ class HashTable {
     // storing value as an key-value pair
     // eslint-disable-next-line no-underscore-dangle
     this._push(index, { key, value });
+
+    /**
+     * calculate the load factor, if it's greater than threshold
+     * resize the hash table
+     */
+    const loadFactor = (this.size / this.slot).toFixed(1);
+    if (loadFactor > this.threshold) {
+      // console.log('Resizing hash table');
+      // eslint-disable-next-line no-underscore-dangle
+      this._resize();
+    }
   }
 
   get(key) {
