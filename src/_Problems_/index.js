793882281bf80c85289b2467c4cfe0bcf079ddf9
TanyaKhandelwal <65648613+TanyaKhandelwal@users.noreply.github.com>
2021-09-04 12:16:12 +0530

    Add files via upload
    Added problem statement and time complexity

--- src/_Problems_/index.js
+++ src/_Problems_/index.js
@@ -1,3 +1,9 @@
+// FIND BALANCED PARENTHESIS
+// FOR '[{()}]' ---->>>>  BALANCED
+// FOR '[{()]' ---->>>>  UNBALANCED
+// Time complexity : O(n)  n is the length of the string provided.
+
+
 function parentheses(s) {
     if(typeof s !== "string" || s.length % 2 !== 0) return false;
     let i = 0;
