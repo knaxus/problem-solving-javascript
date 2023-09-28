5128115b4f4672ddd98517b5ef0a3e40a91a6629
Albert Levonyan <albertlevonyan13@gmail.com>
2023-09-28 15:02:04 +0400

    Added new tests

---
+++ src/_Problems_/Century_fromYear/index.js
@@ -0,0 +1,7 @@
+function getCentury(year) {
+  // Your century calculation logic here
+  return Math.ceil(year / 100);
+}
+
+// Export the getCentury function for testing
+module.exports = getCentury;
